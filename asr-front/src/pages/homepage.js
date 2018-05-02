6fea45a302ae3cfcab02a941a752c7de14950868
kamemos <mosagile@gmail.com>
2018-05-02 21:22:57 +0700

    edit homepage

--- asr-front/src/pages/homepage.js
+++ asr-front/src/pages/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReactMic } from 'react-mic'
+import {Redirect} from 'react-router-dom'
 import io from 'socket.io-client'
 import GifPlayer from 'react-gif-player'
 import '../assets/css/custom.css'
@@ -10,6 +10,8 @@ class HomePage extends React.Component{
         this.state = {
             data : '',
             pepe : 1,
+            wait : false,
+            redirect : false
         }
         this.socket = io('http://localhost:3001/')
     }
@@ -18,33 +20,64 @@ class HomePage extends React.Component{
         this.socket.on('event-kaldi',(data)=>{
             switch(data.msg){
                 case 'ว่าง':
-                    this.setState({data:'ว่าง',pepe:2})
+                    this.setState({data: data.msg})
+                case 'มาละ':
+                    this.setState({data: data.msg})
             }
         })
+        if (this.state.wait){
+            setInterval(()=>{this.setState({redirect:true})},2000)
+        }
     }
     
     render() {
+    const Pepe = () => {
+        if (this.state.data === 'ว่าง'){
+            return (
+                (<GifPlayer style={{borderRadius:'50%',height:"700px",width:"700px",color:'green',marginTop:'20px'}} autoplay={true} gif={require('../assets/gifs/pepe-in-love.gif')} />)
 
-    return (
-        <div className="flexbox-row">
-            {(this.state.pepe === 1) ? 
-                (<GifPlayer style={{borderRadius:'50%',eight:"700px",width:"700px",color:'green'}} autoplay={true} gif={require('../assets/gifs/thinking.gif')} />) :
-                (<GifPlayer style={{borderRadius:'50%',height:"700px",width:"700px",color:'green'}} autoplay={true} gif={require('../assets/gifs/pepe-in-love.gif')} />)
-            }
-            {/* <audio hidden ref="audio_tag" src={require('../assets/sounds/oh-my-god.wav')} controls autoPlay/> */}
-            {(this.state.data !== 'ว่าง') ?
+            )
+        }
+        else if (this.state.data === 'มาละ'){
+            return (
+                (<GifPlayer style={{borderRadius:'50%',height:"700px",width:"700px",color:'green',marginTop:'20px'}} autoplay={true} gif={require('../assets/gifs/pepe-shock.gif')} />)
+            )
+        }
+        else {
+            return (
+                (<GifPlayer style={{borderRadius:'50%',eight:"700px",width:"700px",color:'green',marginTop:'20px'}} autoplay={true} gif={require('../assets/gifs/thinking.gif')} />)
+            )
+        }
+    }   
+    const Text = () =>{
+        if (this.state.data === 'ว่าง'){
+            return (
+                <div className="flexbox-col" style={{padding:'30px'}}>
+                    <h1 style={{fontSize:"300%"}}>LET'S SAY</h1>
+                    <h1 style={{fontSize:"500%"}}>"DJ KALDI"</h1>
+                </div>
+            )
+        }
+        else if (this.state.data === 'มาละ') {
+            return <div/>
+        }
+        else {
+            return (
                 (
                     <div className="flexbox-col" style={{padding:'30px'}}>
-                        <h1 style={{fontSize:"300%"}}>WELCOME TO</h1>
-                        <h1 style={{fontSize:"500%",color:'rgb(4, 114, 4)'}}>DJ KALDI</h1>
-                    </div>
-                ):(
-                    <div className="flexbox-col" style={{padding:'30px'}}>
-                        <h1 style={{fontSize:"300%"}}>LET'S SAY</h1>
-                        <h1 style={{fontSize:"500%",color:'rgb(4, 114, 4)'}}>"DJ KALDI"</h1>
+                        <h1 style={{fontSize:"300%"}}>Welcome to</h1>
+                        <h1 style={{fontSize:"500%"}}>DJ KALDI</h1>
                     </div>
                 )
-            }
+            )
+        }
+    }
+    if (this.state.redirect) return <Redirect to="/playing"/>
+    return (
+        <div className="flexbox-row">
+            <Pepe />
+            {/* <audio hidden ref="audio_tag" src={require('../assets/sounds/oh-my-god.wav')} controls autoPlay/> */}
+            <Text />
         </div>
     );
     }
